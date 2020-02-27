b7f787ec7cbdf0fed5ac5f426d6de4c216992c98
NayanKumarG <nayangkumar@gmail.com>
2020-02-27 09:35:59 +0530

    [Nayan] Add. File to start spring application

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/FundooNotesApiApplication.java
@@ -0,0 +1,18 @@
+/**
+ * @author Nayan Kumar G
+ * purpose : Starts spring application
+ * 
+ */
+package com.bridgelabz.fundoonotes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooNotesApiApplication.class, args);
+		
+	}
+
+}
