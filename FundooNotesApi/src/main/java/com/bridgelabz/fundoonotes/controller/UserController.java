c5563b843c9adfe1096608167f4427d3f94bb120
NayanKumarG <nayangkumar@gmail.com>
2020-02-28 09:26:18 +0530

    [Nayan] Refactor. the delete user api added

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -7,7 +7,10 @@ package com.bridgelabz.fundoonotes.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,44 +19,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.fundoonotes.dto.UserDto;
 import com.bridgelabz.fundoonotes.entity.User;
+import com.bridgelabz.fundoonotes.repository.UserRepository;
 import com.bridgelabz.fundoonotes.service.UserService;
 
 @RestController
 public class UserController {
-
-	private UserService userService;
-
-	/**
-	 * 
-	 * @param userService to initialize
-	 */
+	
 	@Autowired
-	public UserController(UserService userService) {
-		super();
-		this.userService = userService;
-	}
-
+	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
 	@PostMapping(value = "/users/register")
-	public String userRegistration(@RequestBody UserDto userDto)
+	public ResponseEntity<String> userRegistration(@RequestBody UserDto userDto)
 	{
 
 		userService.addUser(userDto);
-		return "Registartion svuccess";
+		return new ResponseEntity<String>("Registration success", HttpStatus.OK);
 
 	}
 	
-	@GetMapping(value = "/users")
-	public List<User> getUsers()
-	{
-		
-		return userService.getUsers();
-	}
-	
+
 	@GetMapping(value = "/user/{userId}")
 	public Optional<User> getUser(@PathVariable long userId)
 	{
 		return userService.getUser(userId);
 	}
+	
+	@GetMapping("/deleteUser/{userId}")
+	public void deleteUser(@PathVariable long userId)
+	{
+		userService.deleteUser(userId);
+	}
+	@GetMapping(value = "/users")
+	public List<User> getUsers()
+	{
+
+		return userService.getUsers();
+	}
+	
+	
+	
 	
 	
 }
