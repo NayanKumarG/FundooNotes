5a442669ab31a16ad71c500624d5d1701905fd61
NayanKumarG <nayangkumar@gmail.com>
2020-02-28 09:28:04 +0530

    [Nayan] Refactor. curd operation using hibernate

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/UserServiceImpl.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/UserServiceImpl.java
@@ -10,7 +10,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,25 +24,28 @@ import com.bridgelabz.fundoonotes.configuration.PasswordSecurityConfiguration;
 import com.bridgelabz.fundoonotes.dto.UserDto;
 import com.bridgelabz.fundoonotes.entity.User;
 import com.bridgelabz.fundoonotes.repository.UserRepository;
+import com.bridgelabz.fundoonotes.utility.JwtUtil;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService{
 	
+	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
 	private User user;
+	
+	@Autowired
 	private BCryptPasswordEncoder bcryptPasswordEncoder;
 	
 	@Autowired
-	public UserServiceImpl(UserRepository userRepository, User user, BCryptPasswordEncoder bcryptPasswordEncoder, PasswordSecurityConfiguration passwordSecurity) {
-		super();
-		this.userRepository = userRepository;
-		this.user = user;
-		this.bcryptPasswordEncoder= bcryptPasswordEncoder;
+	private JwtUtil jwtUtil;
 	
-	}
-
+	@Autowired
+	private EntityManager entityManager;
 
 	@Transactional
 	@Override
@@ -46,7 +53,12 @@ public class UserServiceImpl implements UserService{
 		BeanUtils.copyProperties(userDto , user);
 		user.setDateTime(LocalDateTime.now());
 		user.setPassword(bcryptPasswordEncoder.encode((userDto.getPassword())));
-		userRepository.save(user);
+		String token = jwtUtil.generateToken(userDto);
+		log.info("Generated token:"+token);
+		Session session = entityManager.unwrap(Session.class);
+		session.save(user);
+		
+		//userRepository.save(user);
 	}
 	
 	
@@ -54,16 +66,31 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public List<User> getUsers() {
 	
-		List<User> users = new ArrayList<>();
-		userRepository.findAll().forEach(users::add);
+		Session session = entityManager.unwrap(Session.class);
+		Query<User> query = session.createQuery("from User" , User.class);
+		List<User> users = query.getResultList();
+		//List<User> users = new ArrayList<>();
+		//userRepository.findAll().forEach(users::add);
 		return users;
 	}
 
-
+	@Transactional
 	@Override
 	public Optional<User> getUser(long userId) {
 		
 		return userRepository.findById(userId);
+		
 	}
+
+	@Transactional
+	@Override
+	public void deleteUser(long userId) {
+		Session session = entityManager.unwrap(Session.class);
+		User user = session.get(User.class, userId);
+		session.delete(user);
+	}
+
+
+	
 
 }
