4f0152b9f5b935ded64bf2f2c09d2248d56bcf19
NayanKumarG <nayangkumar@gmail.com>
2020-02-29 18:34:01 +0530

    [Nayan] Refactor. method to verify user added

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
@@ -1,13 +1,14 @@
 /**
  * @author Nayan Kumar G
  * purpose : Interface for user Service
- * 
+ * date    :25-02-2020
  */
 package com.bridgelabz.fundoonotes.service;
 import java.util.List;
 import java.util.Optional;
 
 import com.bridgelabz.fundoonotes.dto.UserDto;
+import com.bridgelabz.fundoonotes.dto.UserLoginDto;
 import com.bridgelabz.fundoonotes.entity.User;
 
 
@@ -16,5 +17,8 @@ public interface UserService {
 	void addUser(UserDto userDto);
 	List<User> getUsers();
 	Optional<User> getUser(long userId);
+	void deleteUser(long userId);
+	boolean updateMailVerification(String token);
+	boolean verifyPassword(UserLoginDto userLoginDto);
 
 }
