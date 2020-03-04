590713ddc18006af67bca3c4bbd5b00ffc9b558e
NayanKumarG <nayangkumar@gmail.com>
2020-03-04 09:32:20 +0530

    [Nayan] Add. Repository for user

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -1,15 +1,17 @@
 /**
  * @author Nayan Kumar G
  * purpose : Repository provides Crud operations for User
- * 
+ * date    :25-02-2020
  */
 package com.bridgelabz.fundoonotes.repository;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import com.bridgelabz.fundoonotes.entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+	
 
+	
 }
