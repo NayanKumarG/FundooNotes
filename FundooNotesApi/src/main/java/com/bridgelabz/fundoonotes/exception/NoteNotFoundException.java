caf429cd24823ea2c4ff6db535eb2623a19c66bd
NayanKumarG <nayangkumar@gmail.com>
2020-03-05 14:20:55 +0530

    [Nayan] Add. file to handle NoteNotfoundException

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/NoteNotFoundException.java
@@ -0,0 +1,27 @@
+/**
+ * @author Nayan Kumar G
+ * purpose :Custom exception for Note Module
+ * date    :04-04-2020
+ */
+package com.bridgelabz.fundoonotes.exception;
+import lombok.Getter;
+
+@Getter
+public class NoteNotFoundException extends RuntimeException{
+	
+	
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	/**
+	 * 
+	 * @param message from exception
+	 */
+	public NoteNotFoundException(String message) {
+		super();
+		this.message = message;
+	}
+	
+	
+
+}
