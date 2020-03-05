1917b6b5ee3ae5e8b6350e5b905bd94e27e96a25
NayanKumarG <nayangkumar@gmail.com>
2020-03-05 14:05:53 +0530

    [Nayan] Add . configuration for swagger

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/configuration/SwaggerConfiguration.java
@@ -0,0 +1,26 @@
+/**
+ * @author Nayan Kumar G
+ * purpose :Configuration for swagger
+ * date    :2-03-2020
+ */
+package com.bridgelabz.fundoonotes.configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+
+public class SwaggerConfiguration {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.fundoonotes.controller"))
+				.paths(PathSelectors.any()).build();
+	}
+
+}
