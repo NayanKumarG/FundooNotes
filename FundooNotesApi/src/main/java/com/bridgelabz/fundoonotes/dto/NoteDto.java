ed4df485feddbc389e1f19e3620056a954cbb265
NayanKumarG <nayangkumar@gmail.com>
2020-03-05 14:07:03 +0530

    [Nayan] Add . dto file for note

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/dto/NoteDto.java
@@ -0,0 +1,15 @@
+/**
+ * @author Nayan Kumar G
+ * purpose :Dto for Note
+ * date    :03-03-2020
+ */
+package com.bridgelabz.fundoonotes.dto;
+import lombok.Data;
+
+@Data
+public class NoteDto {
+	
+private String title;
+private String description;
+	
+}
