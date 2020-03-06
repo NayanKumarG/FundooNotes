2467ef1d484c99a2be51a93d8ca0665b4fa66a0f
NayanKumarG <nayangkumar@gmail.com>
2020-03-06 18:50:08 +0530

    [Nayan] Add. Exception handling for invalid token

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/InvalidTokenException.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/InvalidTokenException.java
@@ -7,10 +7,16 @@ public class InvalidTokenException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	private String message;
+	private String msg = "invalid token Exception";
 
 	public InvalidTokenException(String message) {
 		super();
 		this.message = message;
+	}
+	
+	public String getMessage()
+	{
+		return msg;
 	}
 	
 }
