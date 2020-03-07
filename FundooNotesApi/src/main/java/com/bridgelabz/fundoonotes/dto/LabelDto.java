1ebb22f05c9023d0e9f54bb753d3e80248ae159a
NayanKumarG <nayangkumar@gmail.com>
2020-03-07 18:29:39 +0530

    [Nayan] Add . Dto for label

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/dto/LabelDto.java
@@ -0,0 +1,13 @@
+/**
+ * @author Nayan Kumar G
+ * purpose :Dto for label
+ * date    :06-03-2020
+ */
+package com.bridgelabz.fundoonotes.dto;
+import lombok.Data;
+
+@Data
+public class LabelDto {
+
+	private String labelName;
+}
