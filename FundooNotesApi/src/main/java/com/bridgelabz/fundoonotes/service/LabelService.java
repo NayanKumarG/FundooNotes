534c7371adefcefbf8dca649607281a7e3527769
NayanKumarG <nayangkumar@gmail.com>
2020-03-09 18:34:45 +0530

    [Nayan] Refactor .Service to get list of note by label added

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/LabelService.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/LabelService.java
@@ -4,13 +4,29 @@
  * date    :06-03-2020
  */
 package com.bridgelabz.fundoonotes.service;
+import java.util.List;
+
 import com.bridgelabz.fundoonotes.dto.LabelDto;
 import com.bridgelabz.fundoonotes.dto.LabelUpdateDto;
+import com.bridgelabz.fundoonotes.entity.LabelEntity;
+import com.bridgelabz.fundoonotes.entity.NoteEntity;
 public interface LabelService {
 
 	void createLabel(LabelDto labelDto, String token);
 
 	void updateLabel(LabelUpdateDto labelUpdateDto, String token);
+
+	void deleteLabel(long labelId, String token);
+
+	void removeLabel(long labelId, long noteId, String token);
+
+	void addLabel(long labelId, long noteId, String token);
+
+	List<LabelEntity> getLabels(String token);
+
+	List<NoteEntity> getNotesByLabel(long labelId, String token);
+
+	
 
 	
 }
