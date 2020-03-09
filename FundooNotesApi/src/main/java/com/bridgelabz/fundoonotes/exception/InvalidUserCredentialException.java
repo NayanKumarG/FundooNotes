c379a7b5cb091f12d8767ec5ada2499a4458804d
NayanKumarG <nayangkumar@gmail.com>
2020-03-09 18:42:43 +0530

    [Nayan] Add. InvalidUserCredentialException file added

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/InvalidUserCredentialException.java
@@ -0,0 +1,20 @@
+/**
+ * @author Nayan Kumar G
+ * purpose :To throw invalid user credential exception
+ * date    :09-03-2020
+ */
+package com.bridgelabz.fundoonotes.exception;
+
+public class InvalidUserCredentialException extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidUserCredentialException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
