bc187d8f5143c2fb5d74a8d9d2b15107bc878760
NayanKumarG <nayangkumar@gmail.com>
2020-03-09 18:40:21 +0530

    [Nayan] Refactor. api to get notes by label added

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/controller/LabelController.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/controller/LabelController.java
@@ -4,9 +4,14 @@
  * date    :06-03-2020
  */
 package com.bridgelabz.fundoonotes.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.fundoonotes.dto.LabelDto;
 import com.bridgelabz.fundoonotes.dto.LabelUpdateDto;
+import com.bridgelabz.fundoonotes.entity.LabelEntity;
+import com.bridgelabz.fundoonotes.entity.NoteEntity;
 import com.bridgelabz.fundoonotes.response.Response;
 import com.bridgelabz.fundoonotes.service.LabelService;
 
@@ -38,6 +45,12 @@ public class LabelController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(new Response("Label created" , labelDto));
 	}
 	
+	/**
+	 * Api to update label
+	 * @param labelUpdateDto to hold updating detail
+	 * @param token to identify user
+	 * 
+	 */
 	@PutMapping("/labels/update")
 	public ResponseEntity<Response> updateLabel(@RequestBody LabelUpdateDto labelUpdateDto , @RequestHeader String token)
 	{
@@ -45,4 +58,70 @@ public class LabelController {
 		return ResponseEntity.status(HttpStatus.OK).body(new Response("Label created" , labelUpdateDto));
 	}
 	
+	/**
+	 * Api to delete label
+	 * @param labelId to delete particular label
+	 * @param token to identify user
+	 * 
+	 */
+	@DeleteMapping("/labels/delete")
+	public ResponseEntity<Response> deleteLabel(@RequestParam long labelId , @RequestHeader String token)
+	{
+
+		labelService.deleteLabel(labelId , token);
+		return ResponseEntity.status(HttpStatus.OK).body(new Response("Label deleted" ));
+	}
+	
+	/**
+	 * Api to add label to note
+	 * @param labelId to remove label
+	 * @param noteId to identify note
+	 * @param token to identify user
+	 * 
+	 */
+	@PostMapping("/labels/addLabel")
+	public ResponseEntity<Response> addLabel(@RequestParam long labelId , @RequestParam long noteId , @RequestHeader String token)
+	{
+		labelService.addLabel(labelId , noteId , token);
+		return ResponseEntity.status(HttpStatus.OK).body(new Response("Label added" ));
+	}
+	
+	/**
+	 * Api to rmove label from note
+	 * @param labelId to remove label
+	 * @param noteId to identify note
+	 * @param token to identify user
+	 * 
+	 */
+	@PostMapping("/labels/remove")
+	public ResponseEntity<Response> removeLabel(@RequestParam long labelId , @RequestParam long noteId , @RequestHeader String token)
+	{
+		labelService.removeLabel(labelId , noteId , token);
+		return ResponseEntity.status(HttpStatus.OK).body(new Response("Label removed" ));
+	}
+	
+	/**
+	 * Api to get all labels
+	 * @param token to identify user
+	 * @return list of labels
+	 */
+	@GetMapping("/labels/getLabels")
+	public ResponseEntity<Response> getLabels(@RequestHeader String token)
+	{
+		List<LabelEntity> labels = labelService.getLabels(token);
+		return ResponseEntity.status(HttpStatus.OK).body(new Response("Label fetched" , labels ));
+	}	
+	
+	/**
+	 * Api to get all notes having same label
+	 * @param labelId to get label
+	 * @param token to identify user
+	 * @return list of notes fetched
+	 */
+	@GetMapping("/labels/getNotesByLabel/{labelId}")
+	public ResponseEntity<Response> getNotes(@PathVariable long labelId , @RequestHeader String token)
+	{
+		List<NoteEntity> notes = labelService.getNotesByLabel(labelId , token);
+		return ResponseEntity.status(HttpStatus.OK).body(new Response("Notes fetched" , notes ));
+	}
 }
