f16ab982f17e0af2f2f55ec186556cde6e6071f2
NayanKumarG <nayangkumar@gmail.com>
2020-03-09 18:38:25 +0530

    [Nayan] Refactor. manytomany map with label

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/entity/NoteEntity.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/entity/NoteEntity.java
@@ -5,13 +5,22 @@
  */
 package com.bridgelabz.fundoonotes.entity;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -43,6 +52,9 @@ public class NoteEntity {
 	
 	private String color;
 	
-	
-	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "note_label" ,joinColumns = { @JoinColumn(name = "note_id")} , inverseJoinColumns = {@JoinColumn(name = "label_id")})
+	@JsonBackReference
+	@JsonIgnore
+	private List<LabelEntity> labels;
 }
