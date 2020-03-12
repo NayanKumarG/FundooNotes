6775993c3f68b11ece9a2866d5badd01ac0fa280
NayanKumarG <nayangkumar@gmail.com>
2020-03-12 09:50:24 +0530

    [Nayan] Refactor. added status code

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/InvalidTokenException.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/InvalidTokenException.java
@@ -4,6 +4,8 @@
  * date    :09-03-2020
  */
 package com.bridgelabz.fundoonotes.exception;
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 
 @Getter
@@ -11,12 +13,11 @@ public class InvalidTokenException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	private String message;
-	
-	public InvalidTokenException(String message) {
+	private HttpStatus statusCode;
+
+	public InvalidTokenException(String message, HttpStatus statusCode) {
 		super();
 		this.message = message;
+		this.statusCode = statusCode;
 	}
-	
-
-	
 }
