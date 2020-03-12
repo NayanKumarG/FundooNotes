7a3c88a1a6b25bde3811d5562d6ff9490600a1d4
NayanKumarG <nayangkumar@gmail.com>
2020-03-12 09:56:19 +0530

    [Nayan] Refactor. exception handling while parsing token

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/utility/JwtUtil.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/utility/JwtUtil.java
@@ -5,41 +5,41 @@
  */
 
 package com.bridgelabz.fundoonotes.utility;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import com.bridgelabz.fundoonotes.exception.InvalidTokenException;
+
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.extern.slf4j.Slf4j;
 
 @Component
-@Slf4j
 public class JwtUtil {
 
 
 	private String secretKey = "secret";
-	
-	
+
+
 	public String generateToken(long userId)
 	{
-		
+
 		Claims claims = Jwts.claims().setSubject(Long.toString(userId));
 
 		return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS512, secretKey).compact();
 	}
-	
+
 	public long parseToken(String token)
 	{
 		try
 		{
 			Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
 			return Long.parseLong(body.getSubject());
-		} catch (JwtException | ClassCastException e) {
-            log.error("parsing is not possible");
-			
+		} catch (Exception e) {
+			throw new InvalidTokenException("Invalid token" , HttpStatus.BAD_REQUEST);
+
 		}
-		return 0;
-		
+
 	}
 
 }
