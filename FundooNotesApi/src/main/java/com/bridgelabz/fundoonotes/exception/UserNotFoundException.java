64c46e8d4311a497288cd99758b62c4b4c470a01
NayanKumarG <nayangkumar@gmail.com>
2020-03-12 09:53:56 +0530

    [Nayan] Refactor. added status code

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/UserNotFoundException.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/UserNotFoundException.java
@@ -4,6 +4,7 @@
  * date    :04-03-2020
  */
 package com.bridgelabz.fundoonotes.exception;
+import org.springframework.http.HttpStatus;
 import lombok.Getter;
 @Getter
 public class UserNotFoundException extends RuntimeException{
@@ -11,9 +12,11 @@ public class UserNotFoundException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	private String message;
+	private HttpStatus statusCode;
 
-	public UserNotFoundException(String message) {
-		super();
+	public UserNotFoundException(String message ,HttpStatus statusCode) {
+		
 		this.message = message;
+		this.statusCode = statusCode;
 	}
 }
