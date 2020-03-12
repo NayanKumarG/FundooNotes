3b3461652d7836d0fc67a51442e232cdd763339a
NayanKumarG <nayangkumar@gmail.com>
2020-03-12 09:52:11 +0530

    [Nayan] Refactor. added status code

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/NoteNotFoundException.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/exception/NoteNotFoundException.java
@@ -4,6 +4,8 @@
  * date    :05-03-2020
  */
 package com.bridgelabz.fundoonotes.exception;
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 
 @Getter
@@ -12,14 +14,12 @@ public class NoteNotFoundException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
 	private String message;
+	private HttpStatus statusCode;
 
-	/**
-	 * 
-	 * @param message from exception
-	 */
-	public NoteNotFoundException(String message) {
+	public NoteNotFoundException(String message, HttpStatus statusCode) {
 		super();
 		this.message = message;
+		this.statusCode = statusCode;
 	}
 	
 	
