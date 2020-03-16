a45d83bb6ba83801e11e322619f566b674101fee
NayanKumarG <nayangkumar@gmail.com>
2020-03-16 10:46:43 +0530

    [Nayan] Refactor . service for collaborate added

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
@@ -8,18 +8,32 @@ import java.util.List;
 import com.bridgelabz.fundoonotes.dto.UpdatePasswordDto;
 import com.bridgelabz.fundoonotes.dto.UserDto;
 import com.bridgelabz.fundoonotes.dto.UserLoginDto;
+import com.bridgelabz.fundoonotes.entity.NoteEntity;
 import com.bridgelabz.fundoonotes.entity.User;
 
 
 public interface UserService {
 	
 	User addUser(UserDto userDto);
+	
 	boolean updateMailVerification(String token);
+	
 	boolean verifyLogin(UserLoginDto userLoginDto);
+	
 	boolean isMailExist(String email);
+	
 	boolean confirmMail(String emailId);
+	
 	boolean updatePassword(UpdatePasswordDto updatePasswordDto, String token);
+	
 	List<User> getUsers();
+	
 	User getUser(String token);
+	
+	NoteEntity addCollaborator(long noteId, String email, String token);
+
+	NoteEntity deleteCollaborator(long noteId, String email, String token);
+
+	List<NoteEntity> getCollaboratorNotes(String token);
 	
 }
