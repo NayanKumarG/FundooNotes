3e3adb6aa6907e46de256a5effea5034b87730be
NayanKumarG <nayangkumar@gmail.com>
2020-03-17 11:50:54 +0530

    [Nayan] Refactor .service to get file added

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/ProfileService.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/ProfileService.java
@@ -7,12 +7,15 @@ package com.bridgelabz.fundoonotes.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.s3.model.S3Object;
 import com.bridgelabz.fundoonotes.entity.ProfileEntity;
 
 public interface ProfileService {
 
 	ProfileEntity uploadFileToS3(MultipartFile file, String originalFilename);
 
-	ProfileEntity updateFileInS3(MultipartFile file, String token);
+	ProfileEntity updateFileInS3(String token);
+
+	S3Object getProfilePic(String token);
 
 }
