76c0b967d52a922aaf88645a9a4621e871e95dd4
NayanKumarG <nayangkumar@gmail.com>
2020-03-17 09:33:47 +0530

    [Nayan] Add . Service for user profile

---
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/service/ProfileService.java
@@ -0,0 +1,18 @@
+/**
+ * @author Nayan Kumar G
+ * purpose : Provide services to user profile 
+ * date    :16-03-2020
+ */
+package com.bridgelabz.fundoonotes.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bridgelabz.fundoonotes.entity.ProfileEntity;
+
+public interface ProfileService {
+
+	ProfileEntity uploadFileToS3(MultipartFile file, String originalFilename);
+
+	ProfileEntity updateFileInS3(MultipartFile file, String token);
+
+}
