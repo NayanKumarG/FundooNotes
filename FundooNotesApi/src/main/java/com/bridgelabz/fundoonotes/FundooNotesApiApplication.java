c9582fee06be7c7354c08272c23c931d1decabcc
NayanKumarG <nayangkumar@gmail.com>
2020-03-17 11:41:53 +0530

    [Nayan] Add . File to start apring appilication

--- FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/FundooNotesApiApplication.java
+++ FundooNotesApi/src/main/java/com/bridgelabz/fundoonotes/FundooNotesApiApplication.java
@@ -8,6 +8,8 @@ package com.bridgelabz.fundoonotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 public class FundooNotesApiApplication {
 
